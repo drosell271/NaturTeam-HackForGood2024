b06423feac2a5ee4e3dcc2678159fb60d5579dd3
Daniel <drmoreno271@gmail.com>
2024-03-15 18:55:20 +0100

    db-created

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,3 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from typing import Optional
+import uuid
 
 app = FastAPI()
+
+class JSON_Event_Input(BaseModel):
+	name: str
+	description: str
+	image: Optional[str] = None
+	latitude: float
+	longitude: float
+	creator_id: int
+
+class JSON_Event_Output(BaseModel):
+	id: int
+	name: str
+	description: str
+	image: str
+	latitude: float
+	longitude: float
+	creator_id: int
+	created_at: datetime
+	users_attending: int
+	finished: bool
+
+class JSON_Event_GPS(BaseModel):
+	id: int
+	latitude: float
+	longitude: float
+
+class JSON_Login(BaseModel):
+	username: str
+	password: str
+
+
+@app.post("/login")
+async def login(json: JSON_Login):
+	pass
